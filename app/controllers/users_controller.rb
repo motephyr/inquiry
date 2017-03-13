b5bf06ae62fec6f6376c2a008d20c9844f2291c4
motephyr <motephyr@gmail.com>
2017-03-13 21:07:09 +0800

    修正個人設定&密碼頁

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,25 @@
+class UsersController < ApplicationController
+
+  before_action :authenticate_user!
+
+  def edit_password
+    @user = User.find(current_user.id)
+  end
+
+  def update_password
+    @user = User.find(current_user.id)
+    if @user.update(user_params)
+      # Sign in the user by passing validation in case their password changed
+      sign_in @user, :bypass => true
+      redirect_to root_path
+    else
+      render "edit_password"
+    end
+  end
+
+  def user_params
+    # NOTE: Using `strong_parameters` gem
+    params.require(:user).permit(:password, :password_confirmation)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,18 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'registrations' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
 
   resources :welcome do
     collection do
       get 'demo'
+    end
+  end
+
+  resources :users, only: [] do
+    collection do
+      get 'edit_password'
+      patch 'update_password'
     end
   end
 end
