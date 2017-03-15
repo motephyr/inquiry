4b56625fc7d455252cc769d3e1ce575986630d2a
KinoLien <miecowbai@gmail.com>
2017-03-15 20:04:58 +0800

    Price controller update consistent.

--- app/controllers/appraisal_prices_controller.rb
+++ app/controllers/appraisal_prices_controller.rb
@@ -1,4 +1,6 @@
 class AppraisalPricesController < ApplicationController
+  before_action :login_required
+  before_action :price_owner, only: [:update]
 
   def create
     @appraisal_price = current_user.appraisal_prices.build(allowed_params)
@@ -8,7 +10,6 @@ class AppraisalPricesController < ApplicationController
   end
 
   def update
-    @appraisal_price = AppraisalPrice.find(params[:id])
 
     @appraisal_price.update(allowed_params)
     redirect_to @appraisal_price.appraisal
@@ -16,6 +17,14 @@ class AppraisalPricesController < ApplicationController
 
   def allowed_params
     params.require(:appraisal_price).permit(:price, :appraisal_id)
+  end
+
+  def price_owner
+    @appraisal_price = AppraisalPrice.find(params[:id])
+    unless @appraisal_price.user_id == current_user.id
+      flash[:notice] = '你不是這個留言的使用者'
+      redirect_to appraisal_path(@appraisal_price.appraisal)
+    end
   end
 
 end
