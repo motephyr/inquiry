4baa21a7d99a0d1ab35f4918bcacb76b24a945b3
motephyr <motephyr@gmail.com>
2017-03-16 17:56:24 +0800

    fix migration

--- db/migrate/20170314121249_add_count_to_appraisal.rb
+++ db/migrate/20170314121249_add_count_to_appraisal.rb
@@ -2,12 +2,12 @@ class AddCountToAppraisal < ActiveRecord::Migration[5.0]
   def change
     add_column :appraisals, :appraisal_messages_count, :integer, :default => 0
 
-    Appraisal.pluck(:id).each do |i|
-      Appraisal.reset_counters(i, :appraisal_messages) # 全部重算一次
-    end
+    # Appraisal.pluck(:id).each do |i|
+    #   Appraisal.reset_counters(i, :appraisal_messages) # 全部重算一次
+    # end
     add_column :appraisals, :appraisal_prices_count, :integer, :default => 0
-    Appraisal.pluck(:id).each do |i|
-      Appraisal.reset_counters(i, :appraisal_prices) # 全部重算一次
-    end
+    # Appraisal.pluck(:id).each do |i|
+    #   Appraisal.reset_counters(i, :appraisal_prices) # 全部重算一次
+    # end
   end
 end
