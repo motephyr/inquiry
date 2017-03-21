8cab245af4c0ce9200ab5b6a3742446e913725ac
motephyr <motephyr@gmail.com>
2017-03-21 09:09:56 +0800

    fix asset

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,20 +9,19 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-if ENV['RAILS_ENV'] == 'development'
-  Rails.application.config.assets.precompile << Proc.new do |path|
-    if path =~ /\.(css|js)\z/ && !path.include?('bootstrap')
-      full_path = Rails.application.assets.resolve(path)
-      count = Rails.application.config.assets.paths.select {|app_assets_path|  full_path.starts_with? app_assets_path.to_s }.length
-      if count > 0
-        puts "including asset: " + full_path
-        true
-      else
-        puts "excluding asset: " + full_path
-        false
-      end
+Rails.application.config.assets.precompile << Proc.new do |path|
+  if path =~ /\.(css|js)\z/ && !path.include?('bootstrap')
+    @assets ||= Rails.application.assets || Sprockets::Railtie.build_environment(Rails.application)
+    full_path = @assets.resolve(path)
+    count = Rails.application.config.assets.paths.select {|app_assets_path|  full_path.starts_with? app_assets_path.to_s }.length
+    if count > 0
+      puts "including asset: " + full_path
+      true
     else
+      puts "excluding asset: " + full_path
       false
     end
+  else
+    false
   end
 end
