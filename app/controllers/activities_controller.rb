d2a97d5af02de2bd838b74386a533801b0ca2706
motephyr <motephyr@gmail.com>
2017-03-23 17:28:28 +0800

    修正通知layout.

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,6 +1,6 @@
 class ActivitiesController < ApplicationController
   def index
-    @activities = PublicActivity::Activity.order("created_at desc").where(recipient_type: "User", recipient_id: current_user.id)
+    @activities = PublicActivity::Activity.includes(:owner, trackable: :appraisal).order("created_at desc").where(recipient_type: "User", recipient_id: current_user.id).limit(10)
     respond_to do |f|
       f.js
     end
--- app/helpers/activities_helper.rb
+++ app/helpers/activities_helper.rb
@@ -1,2 +1,5 @@
 module ActivitiesHelper
+  def activity_created_at(activity)
+    activity.created_at.strftime("%Y-%m-%d %H:%M")
+  end
 end
