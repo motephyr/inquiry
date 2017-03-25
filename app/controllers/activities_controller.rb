d500b6d383a0d0c63c0591acc153489f8e869f1a
motephyr <motephyr@gmail.com>
2017-03-25 22:44:28 +0800

    add cache & fix

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,6 +1,6 @@
 class ActivitiesController < ApplicationController
   def index
-    @activities = PublicActivity::Activity.includes(:owner, trackable: :appraisal)
+    @activities = PublicActivity::Activity.includes(:owner, :trackable)
     .where(recipient_type: "User", recipient_id: current_user.id)
     .order("created_at desc")
     .limit(10)
