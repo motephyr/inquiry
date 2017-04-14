ea39e5dac3a6957f365a774c73f7be47e774fa2a
motephyr <motephyr@gmail.com>
2017-04-14 15:56:58 +0800

    try to fix migration.

--- db/migrate/20170413060105_add_cares_count_to_work.rb
+++ db/migrate/20170413060105_add_cares_count_to_work.rb
@@ -3,8 +3,8 @@ class AddCaresCountToWork < ActiveRecord::Migration[5.0]
     add_column :works, :cares_count, :integer, default: 0
 
     # Work.reset_column_information
-       Work.all.each do |p|
-          Work.update_counters p.id, :cares_count => p.cares.length
-        end
+       # Work.all.each do |p|
+       #    Work.update_counters p.id, :cares_count => p.cares.length
+       #  end
   end
 end
