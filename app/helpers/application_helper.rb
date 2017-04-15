bed5fe687e13ef3edae43d310d9c8b5ad89f68c3
motephyr <motephyr@gmail.com>
2017-04-16 01:19:23 +0800

    for every area put some text to easy edit work.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -43,22 +43,22 @@ module ApplicationHelper
   def render_resolve_url(work)
     url = work.attach_url
     if (image_match = /\.(jpg|jpeg|tiff|png|gif|bmp)$/.match(url)) and image_match.present?
-    	# image matches
-    	content_tag :img, '', src: "#{url}"
+      # image matches
+      content_tag :img, '', src: "#{url}"
     elsif (audio_match = /\.(wav|mp3|wma|midi|aif|aifc|aiff|au|ea)$/.match(url)) and audio_match.present?
-    	# audio matches
-    	content_tag :audio, '', controls: "controls", src: "#{url}", style: "width:100%;"
+      # audio matches
+      content_tag :audio, '', controls: "controls", src: "#{url}", style: "width:100%;"
     elsif (video_match = /\.(mp4|webm|ogg)$/.match(url)) and video_match.present?
-    	# video matches
-    	content_tag :video, '', controls: "controls", src: "#{url}", width: "100%", height: "100%"
+      # video matches
+      content_tag :video, '', controls: "controls", src: "#{url}", width: "100%", height: "100%"
     elsif (youtube_match = /^(?:https?:\/\/)?(?:m\.|www\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=))((\w|-){11})(\S*)?$/.match(url)) and youtube_match.present?
-    	# youtube matches
-    	content_tag :iframe, '', src: "https://www.youtube.com/embed/#{youtube_match[1]}?rel=0", width: '100%', height:'100%', frameborder: '0'
+      # youtube matches
+      content_tag :iframe, '', src: "https://www.youtube.com/embed/#{youtube_match[1]}?rel=0", width: '100%', height:'100%', frameborder: '0'
     else
-    	# others
-    	content_tag :div, :data => { :remote_url => url }, class: "remote-preview" do
+      # others
+      content_tag :div, :data => { :remote_url => url }, class: "remote-preview" do
         content_tag(:div,'', style: 'background-image:url(' + work.remote_image_url + ')', class: 'preview-image')  +
-        content_tag(:p, work.remote_description,  class: 'preview-description')
+          content_tag(:p, work.remote_description,  class: 'preview-description')
       end
     end
   end
@@ -74,10 +74,10 @@ module ApplicationHelper
   end
 
   def render_with_p_tags(str)
-  	str.gsub!(/\r\n?/, "\n")
-  	lines = str.split("\n")
-  	lines.collect {|line| concat(content_tag(:p, line.presence || "&nbsp;".html_safe))}
-  	""
+    str.gsub!(/\r\n?/, "\n")
+    lines = str.split("\n")
+    lines.collect {|line| concat(content_tag(:p, line.presence || "&nbsp;".html_safe))}
+    ""
   end
 
   def rasf(text)
@@ -95,4 +95,26 @@ module ApplicationHelper
     end
   end
 
+  def ckeditor_content(user)
+    if user.user_info.category.title == '工業設計'
+      "製作​​​​​​​時間： \n\n準備素材： \n\n使用工具：                \n\n參考文件或連結：\n\n"
+    elsif user.user_info.category.title == '美術設計'
+      "製作​​​​​​​時間： \n\n內容簡介： \n\n                        \n\n參考文件或連結：\n\n"
+    elsif user.user_info.category.title == '文字編輯'
+      "製作​​​​​​​時間： \n\n概念發想： \n\n                        \n\n參考文件或連結：\n\n"
+    elsif user.user_info.category.title == '程式開發'
+      "製作​​​​​​​時間： \n\n功能簡介： \n\n使用的language和library： \n\n參考文件或連結：\n\n"
+    elsif user.user_info.category.title == '行銷企畫'
+      "製作​​​​​​​時間： \n\n概念發想： \n\n                        \n\n參考文件或連結：\n\n"
+    elsif user.user_info.category.title == '音樂人'
+      "製作​​​​​​​時間： \n\n準備素材： \n\n使用工具：                \n\n參考文件或連結：\n\n"
+    elsif user.user_info.category.title == '影像製作'
+      "製作​​​​​​​時間： \n\n準備素材： \n\n使用工具：                \n\n參考文件或連結：\n\n"
+    elsif user.user_info.category.title == '財務會計'
+      "描述你的工作成果..."
+    elsif user.user_info.category.title == '其他'
+      "描述你的工作成果..."
+    end
+
+  end
 end
