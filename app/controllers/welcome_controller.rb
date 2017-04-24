9f47df1305ca62cb523091f9bac022cb3039d07e
motephyr <motephyr@gmail.com>
2017-04-24 21:42:19 +0800

    add feature work to landing page.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,8 @@ class WelcomeController < ApplicationController
 
   def index
     cookies["new_viewer"] = 'false'
+    @works = Work.includes(:user, :cares).is_published.is_featured.order_by_new.limit(8)
+
     render layout: "landing_page"
   end
 
