14aa69bd97ac259e3b7c2ff468add8cdd47cf4a9
KinoLien <miecowbai@gmail.com>
2017-04-27 11:38:35 +0800

    Add parameters for pre-fill form.

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -4,9 +4,18 @@ class Users::RegistrationsController < Devise::RegistrationsController
     render layout: "account"
   end
 
+  def new
+  	@user = User.new(user_params)
+  end
+
   protected
 
   def update_resource(resource, params)
     resource.update_without_password(params)
+  end
+
+  private
+  def user_params
+  	params.permit(:email, :name)
   end
 end
