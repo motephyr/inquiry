84b3983e76b37622f108291a82398f24f6bb17b3
motephyr <motephyr@gmail.com>
2017-05-11 11:53:05 +0800

    test

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-# ApplicationController.renderer.defaults.merge!(
-#   http_host: 'example.org',
-#   https: false
-# )
+ApplicationController.renderer.defaults.merge!(
+  http_host: Setting.domain_name,
+  https: false
+)
