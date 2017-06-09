9a7f5715101781864faeb71cbe7c633fede5cc74
KinoLien <miecowbai@gmail.com>
2017-06-09 13:39:57 +0800

    All images should through image load handler.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -71,7 +71,7 @@ module ApplicationHelper
     obj = get_resolved_url_obj(url)
     case obj[:type]
     when "image"
-      tag :img, src: "#{url}"
+      tag :img, src: "#{url}", onload: "onImageLoad(this)"
     when "audio"    #compare to 2
       tag :audio, controls: "controls", src: "#{url}", style: "width:100%;"
     when "video"
@@ -110,7 +110,7 @@ module ApplicationHelper
     image_match = /\.(jpg|jpeg|tiff|png|gif|bmp)$/i.match(file)
     audio_match = /\.(wav|mp3|wma|ogg|midi|aif|aifc|aiff|au|ea)$/i.match(file)
     if image_match.present?
-      tag :img, src: "#{file}"
+      tag :img, src: "#{file}", onload: "onImageLoad(this)"
     elsif audio_match.present?
       content_tag :audio, '', controls: "controls", src: "#{file}"
     end
