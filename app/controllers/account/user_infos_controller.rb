eb2bade233c81289243bc7a34bb9659f422cb69a
motephyr <motephyr@gmail.com>
2017-10-06 16:25:06 +0800

    photoswipe.

--- app/controllers/account/user_infos_controller.rb
+++ app/controllers/account/user_infos_controller.rb
@@ -25,8 +25,8 @@ class Account::UserInfosController < ApplicationController
       never_edit_user_info
     end
     @works = (@user == current_user) ? @user.works.includes(:cares).order("is_published desc").order_by_new : @user.works.includes(:cares).is_published.order_by_new
-    filter_works = @works.select{|x| x.attach_avatar.present?}
-    @main_image_url = filter_works.length > 0 ? filter_works[rand(0...filter_works.length)].attach_avatar.url : ActionController::Base.helpers.asset_path('top_banner.jpg')
+    @filter_works = @works.select{|x| x.attach_avatar.present?}
+    @main_image_url = @filter_works.length > 0 ? @filter_works[rand(0...@filter_works.length)].attach_avatar.url : ActionController::Base.helpers.asset_path('top_banner.jpg')
   end
 
   def personal_page
